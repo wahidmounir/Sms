a419d68ed1fa0bc4963e2a62414c44cb434f37b1
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2015-05-09 13:52:54 +0500

    Created the default gateway factory interface and implementation.

---
+++ Factory/GatewayFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is a part of the Yoqut package.
+ *
+ * (c) Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that is distributed with this source code.
+ */
+
+namespace Yoqut\Component\Sms\Factory;
+
+use Yoqut\Component\Sms\Model\Gateway;
+
+/**
+ * The default gateway factory implementation
+ *
+ * @author Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
+ */
+class GatewayFactory implements GatewayFactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public static function create($host, $port, $username, $password)
+    {
+        return new Gateway($host, $port, $username, $password);
+    }
+}
---
+++ Factory/GatewayFactoryInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is a part of the Yoqut package.
+ *
+ * (c) Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that is distributed with this source code.
+ */
+
+namespace Yoqut\Component\Sms\Factory;
+
+use Yoqut\Component\Sms\Model\GatewayInterface;
+
+/**
+ * The gateway factory interface
+ *
+ * @author Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
+ */
+interface GatewayFactoryInterface
+{
+    /**
+     * Creates a new gateway
+     *
+     * @param string $host
+     * @param string $port
+     * @param string $username
+     * @param string $password
+     * @return GatewayInterface
+     */
+    public static function create($host, $port, $username, $password);
+}
